97c176713109c38ab21f19f9ea8a31b62ab28ea2
KViktorK <72447099+KViktorK@users.noreply.github.com>
2021-02-03 16:42:07 +0100

    +

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,7 +7,7 @@ if (animItems.length > 0) {
             const animItem = animItems[index]
             const animItemHeight = animItem.offsetHeight
             const animItemOffSet = offset(animItem).top
-            const animStart = 4
+            const animStart = 1
             let animItemPoint = window.innerHeight - animItemHeight / animStart
             if (animItemHeight > window.innerHeight) {
                 animItemPoint = window.innerHeight - window.innerHeight / animStart
